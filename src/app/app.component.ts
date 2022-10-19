56aa6da99f7e3a30a96e4e1f978f53accfcf7194
Sentient Systems <sentientsystems@outlook.com>
2022-10-19 12:02:24 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Example';
+  title = 'Program for Get,Post and Delete in local storage.';
+  constructor(public localStorageService: LocalStorageService) { }
+  firstname: string = '';
+  lastname: string = '';
+  age: number = 1;
+
+  queryKey: string = '';
+  removeKey: string = '';
+
+  queryResult: any = '';
+
+  public person = { firstName: this.firstname, lastName: this.lastname, age: this.age }
+
+  public addPerson() {
+    this.person.firstName = this.firstname
+    this.person.age = this.age;
+    this.person.lastName = this.lastname;
+    this.localStorageService.setItem(this.firstname, JSON.stringify(this.person))
+  }
+  getPerson(nameToRemove: any) {
+    this.queryResult = this.localStorageService.getItem(nameToRemove);
+  }
+  deletePerson(nameToDelete: string) {
+    this.localStorageService.removeItem(nameToDelete);
+  }
+
 }
